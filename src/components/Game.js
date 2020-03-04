8773885e66e140aa093d0863ed9db2a14abe7799
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-03-05 02:24:52 +0530

    introducing state into game component
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Board from './Board'
 
-export default function Game() {
-  return(
-    <div className= "game">
-      <div className = "game-board">
-        <Board/>
+export default class Game extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      xIsNext: true,
+      stepNumber: 0,
+      history: [
+        {squares: Array(9).fill(null)}      
+      ]
+    }
+  }  
+  render() {
+    return(
+      <div className= "game">
+        <div className = "game-board">
+          <Board/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
